ab99bb4d3aa3a6fe14344c092c3c6fcb33e079d5
geugenio <geugnio12@gmail.com>
2025-11-23 16:21:47 -0300

    feat(usuario-service): adicionar service para usuario

---
+++ api/src/main/java/com/lp2/bibliotecaapi/service/UsuarioService.java
@@ -0,0 +1,54 @@
+package com.lp2.bibliotecaapi.service;
+
+import com.lp2.bibliotecaapi.dto.UpdateUsuarioDTO;
+import com.lp2.bibliotecaapi.model.Usuario;
+import com.lp2.bibliotecaapi.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    public UsuarioRepository ur;
+
+    public List<Usuario> findAll(){
+        return ur.findAll();
+    }
+
+    public Optional<Usuario> findById(Long id){
+        return ur.findById(id);
+    }
+
+    public Usuario save(Usuario usuario){
+        ur.save(usuario);
+        return usuario;
+    }
+
+    public boolean delete(Long id){
+        return ur.delete(id);
+
+    }
+
+    public Optional<Usuario> update(long id, UpdateUsuarioDTO dto){
+        Optional<Usuario> existente = ur.findById(id);
+        if(existente.isPresent()){
+            Usuario usuario = existente.get();
+            if(dto.getNome() !=null){
+                usuario.setNome(dto.getNome());
+            }
+            if(dto.getEmail() !=null){
+                usuario.setEmail(dto.getEmail());
+            }
+
+            ur.saveList();
+            return Optional.of(usuario);
+        }
+        return Optional.empty();
+    }
+
+
+
+}
